190959170d50546cc1ab444129f9d63dd16785bb
AtaS <atasasmaz@gmail.com>
2023-12-11 11:56:52 +0000

    Update dev-server-entrypoint.sh

--- dev-server-entrypoint.sh
+++ dev-server-entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Check if php-fpm is running
-if ! pgrep "php-fpm" > /dev/null; then
+if ! pgrep "php-fpm" >/dev/null; then
     php-fpm -D
 fi
 
 # Check if nginx is running
-if ! pgrep "nginx" > /dev/null; then
+if ! pgrep "nginx" >/dev/null; then
     service nginx start
 fi
 
-echo "Dev server should be running at http://localhost:8001 or any other port if you changed the default."
+echo "Dev server is started."
 
 tail -f /dev/null
